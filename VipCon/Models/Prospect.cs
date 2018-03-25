b5616e7462ea2383e205f5fc7ac1abb20080205f
Rodrigo Chamma Castro <rodrigo.chamma@qi3.com.br>
2018-03-25 17:21:47 -0300

    Acerto simulação

--- VipCon/Models/Prospect.cs
+++ VipCon/Models/Prospect.cs
@@ -13,14 +13,15 @@ namespace VipCon.Models
         [Required(ErrorMessage = "É obrigatório informar o nome")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "É obrigatório informar um telefone")]
-        [DataType(DataType.PhoneNumber)]
-        [Phone(ErrorMessage = "Formato de telefone inválido")]        
+        [Required(ErrorMessage = "É obrigatório informar um telefone")]                
+        [RegularExpression(@"^[0-9]{10,11}$", ErrorMessage = "O telefone deve conter no mínimo 10 e no máximo 11 caracterres. ex: 27988887777")]        
         public string Telefone { get; set; }
 
+        [EmailAddress(ErrorMessage = "Digite um email válido")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "É obrigatório informar a modalidade do consórcio")]
+        [Display(Name = "Modalidade")]
         public string ModalidadeConsorcio { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy HH:mm}")]
